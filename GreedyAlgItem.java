f77c4715ccb38822043456bc7628641b2560c87b
g-c-cousseau <56159223+g-c-cousseau@users.noreply.github.com>
2020-01-10 15:05:11 +0100

    Item class

---
+++ GreedyAlgItem.java
@@ -0,0 +1,12 @@
+public class GreedyAlgItem
+{
+    public GreedyAlgItem(String name, double weight, double value, int quantity, float density)
+    {
+        this.name = name;
+        this.weight = weight;
+        this.value = value;
+        this.quantity = quantity;
+        this.density = density;
+    }
+
+}
